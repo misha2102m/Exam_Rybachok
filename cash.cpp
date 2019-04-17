cc39f13f90fd31ca1785de6d9f40865f743b940e
anonymous <user@step.itstep.rv.ua>
2019-04-17 19:03:15 +0300

    Cor Grn Fnction

--- cash.cpp
+++ cash.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-using namespace std
+using namespace std;
+double corgrn();
 int main()
 {
 
 system("pause");
 return 0;
 }
+double corgrn()
+{
+	double grn;
+	cout << "Enter correct grn ( >= 0 , !=abc)";
+	do
+	{
+		cin >> grn;
+		if ((grn >= 'a' && grn <= 'z') || (grn >= 'A' && grn <= 'Z') || grn <= 0)
+		{
+			cout << "Error!\n";
+			return 0;
+		}
+		
+		else if (grn >= 0)
+			break;
+	} while (true);
+	return grn;
+}
+
