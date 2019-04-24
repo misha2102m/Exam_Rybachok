7095d0e11a4b50680521bc14ed24814b7fd496c2
artur <artur.talimonchyk@gmail.com>
2019-04-24 20:13:56 +0300

    Correct grn function

--- cash.cpp
+++ cash.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
+double corgrn();
 int main()
 {
+	
 	system("pause");
 	return 0;
+}
+double corgrn()
+{
+	double grn;
+	cout << "Enter correct grn ( >= 0 , !=abc) ";
+	while (true)
+	{
+		cin >> grn;
+		if (!cin || grn <= 0)
+		{
+			cin.clear();
+			cout << "Incorect input\n";
+			while (cin.get() != '\n')
+			{
+				continue;
+			}
+			continue;
+		}
+		break;
+	}
+	return grn;
 }
