054e10fe35e4a893fe34d367f047b696c652e457
artur <artur.talimonchyk@gmail.com>
2019-04-24 20:14:45 +0300

    Count grns function

--- cash.cpp
+++ cash.cpp
@@ -1,12 +1,58 @@
 #include <iostream>
 using namespace std;
 double corgrn();
+void coins(double grn);
 int main()
 {
-	
 	system("pause");
 	return 0;
 }
+void coins(double grn)
+{
+	int amount25 = 0, amount5 = 0, amount10 = 0, amount2 = 0, amount1 = 0;
+	int grn1 = (int)grn;
+	double temp1 = grn - grn1;
+	double coins;
+	coins = temp1 * 100;
+	int coinsall = (grn1 * 100) + coins;
+	int temp2 = coinsall;
+	int temp3 = 0;
+	int temp;
+	amount25 = coinsall / 25;
+	cout << "n25 = " << amount25 << endl;
+
+	temp = amount25 * 25;
+	temp3 += temp;
+	coinsall = coinsall - temp;
+	amount10 = coinsall / 10;
+	cout << "n10 = " << amount10 << endl;
+
+	temp = amount10 * 10;
+	temp3 += temp;
+	coinsall = coinsall - temp;
+	amount5 = coinsall / 5;
+	cout << "n5 = " << amount5 << endl;
+
+	temp = amount5 * 5;
+	temp3 += temp;
+
+	coinsall = temp2 - temp3;
+	while (true)
+	{
+		if (coinsall > 0)
+		{
+			amount1++;
+			coinsall--;
+		}
+		else
+		{
+			break;
+		}
+	}
+	cout << "n1 = " << amount1 << endl;
+	return;
+}
+
 double corgrn()
 {
 	double grn;
